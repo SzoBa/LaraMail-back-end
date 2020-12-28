d38f475caef4e0e6590118e7eee38355d876b817
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-12-28 11:54:23 +0100

    Add database setup, route testing

--- app/Models/Mail.php
+++ app/Models/Mail.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Mail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_user_from',
+        'id_user_to',
+        'subject',
+        'message',
+    ];
+
+//    protected $casts = [
+//        'sent' => 'datetime',
+//    ];
+    protected $attributes = ['is_read' => false];
+    protected $hidden = [
+        'id',
+    ];
+
+    public $timestamps = false;
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Mail;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/register', function () {
+   //TODO check and save to db
+    $mail = Mail::create(['id_user_from' => 0, 'id_user_to'=>0, 'subject' =>'test subject',
+        'message'=>'something test']);
+    return $mail;
+});
+
+//CRUD
+//get all GET
+//create one POST
+//get one GET /{id}
+//update one PUT /{id}
+//delete DELETE /{id}
+
+//create database and migrations, create model (create service - Eloquent ORM), create controller for info retrieval, return info
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
