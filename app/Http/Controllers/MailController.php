516b79afda8d633a713196a34503fd6c64b89db9
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-06 16:26:08 +0100

    Add validation and status codes

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mail;
-use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
 class MailController extends Controller
 {
+    /*There could be any kind of methods, named in the route.
+    */
+
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -23,69 +27,64 @@ class MailController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        $validation = Validator::make($request->all(),
-            ['id_user_from' => 'required|numeric', 'id_user_to' =>'required|numeric',
-                'subject' => 'required', 'message' => 'required']);
+        $rules = ['id_user_from' => 'required|numeric', 'id_user_to' =>'required|numeric',
+            'subject' => 'required|min:3', 'message' => 'required|min:3'];
+        $validation = Validator::make($request->all(), $rules);
         if ($validation->fails()) {
-            return response('Wrong data inserted!', 400);
+            return response($validation->errors(), 400);
         }
-        return Mail::create($request->all());
-
-//        try {
-//                $mail = Mail::create($request->all());
-//                return response($mail, 201);
-//            } catch (QueryException $error) {
-//                return response('Wrong data inserted!', 400);}
+        return response(Mail::create($request->all()), 201);
+        //this could be with arrow function too, like ->json(data, status)
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
-    public function show($id)
+    public function show(int $id): Response
     {
+        $mail = Mail::find($id);
+        if(is_null($mail)) {
+            return response(['message'=>'Mail not found'], 404);
+        }
         return Mail::find($id);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $id
+     * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id): ?Response
     {
-        try {
-            $mail = Mail::find($id);
-            if ($mail === null) {
-                return response("Id doesn't exist in database!", 400);
-            }
-            $mail->update($request->all());
-            return $mail;
-        } catch (QueryException $error) {
-            return response("Wrong data insert attempt!", 400);
+        $mail = Mail::find($id);
+        if (is_null($mail)) {
+            return response(["message" =>"Id doesn't exist in database!"], 404);
         }
+        $mail->update($request->all());
+        return response($mail, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
-    public function destroy($id)
+    public function destroy(int $id): Response
     {
         $deletedRows = Mail::destroy($id);
         //number of deleted rows
-        return $deletedRows === 0 ? response("No deletion executed!", 400) :
-            response("Record deleted!", 200);
+        return $deletedRows === 0 ? response(["message"=>"No deletion executed!"], 404) :
+            response(["message"=> "Record deleted!"], 204);
     }
 }
