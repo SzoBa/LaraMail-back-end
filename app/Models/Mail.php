652abe6a8097cbd4c3043a7c2945a8f28a519ac5
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-06 16:26:29 +0100

    Add comment

--- app/Models/Mail.php
+++ app/Models/Mail.php
@@ -9,11 +9,15 @@ class Mail extends Model
 {
     use HasFactory;
 
+    //protected $table = 'tablename'; you can define another name for the table, if differs
+
     protected $fillable = [
         'id_user_from',
         'id_user_to',
         'subject',
         'message',
+        'is_read',
+        'sent',
     ];
 
 //    protected $casts = [
@@ -22,9 +26,12 @@ class Mail extends Model
 //if you want to change the type of the returned value
 
     protected $attributes = ['is_read' => false];
-    protected $hidden = [
-        'id',
-    ];
+
+//    protected $hidden = [
+//        'id',
+//    ];
+//If you need to hide an attribute
 
     public $timestamps = false;
+    //if you need to override built-in times
 }
