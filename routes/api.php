a2c1b63f7a1d3ef1c9e7fe7a6aadde2398990b54
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-06 19:04:39 +0100

    Add Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\MailController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Mail;
+use Illuminate\Support\Facades\Hash;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,26 +16,33 @@ use App\Models\Mail;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/register', function () {
-   //TODO check and save to db
-    $mail = Mail::create(['id_user_from' => 0, 'id_user_to'=>0, 'subject' =>'test subject',
-        'message'=>'something test']);
-    return $mail;
-});
-
-//CRUD
-//get all GET
-//create one POST
-//get one GET /{id}
-//update one PUT /{id}
-//delete DELETE /{id}
-
-//create database and migrations, create model (create service - Eloquent ORM), create controller for info retrieval, return info
-
-
-
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//create database and migrations, create model (create service - Eloquent ORM), create controller for info retrieval, return info
+Route::apiResource('mail', MailController::class)
+    ->middleware('auth:sanctum');
+//TODO insert force delete and list trashed mails, draft mails routes
+
+
+
+//Hash or bcrypt also good
+//Route::post('/register', function (Request $request) {
+//    $user = User::create(['name' => 'Test Elek', 'email' => 'test@test.com', 'password' => Hash::make('mypassword')]);
+//    $token = $user->createToken("LaraMailAccess");
+//    return response(["message" => 'Registered','token' => $token->plainTextToken], 201);
+//});
+//
+//Route::post('/login', function () {
+//    $credentials = request()->only(['email', 'password']);
+//    return auth()->attempt($credentials);
+//});
+//ezt a tokent le kell menteni és header Authorization Bearer idebe
+
+//authenticate - it is now used with arrow function at apiResources
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
