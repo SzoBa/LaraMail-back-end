6b3f57d4dd05411febb7c80391f8202f1be1f3ba
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-07 02:11:49 +0100

    Refactor routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\RegisterUserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,33 +18,23 @@ use Illuminate\Support\Facades\Hash;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 //create database and migrations, create model (create service - Eloquent ORM), create controller for info retrieval, return info
 Route::apiResource('mail', MailController::class)
     ->middleware('auth:sanctum');
 //TODO insert force delete and list trashed mails, draft mails routes
 
+Route::post('/register', [RegisterUserController::class, 'store']);
 
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LoginController::class, 'destroy'])
+    ->middleware('auth:sanctum');
 
 //Hash or bcrypt also good
-//Route::post('/register', function (Request $request) {
-//    $user = User::create(['name' => 'Test Elek', 'email' => 'test@test.com', 'password' => Hash::make('mypassword')]);
-//    $token = $user->createToken("LaraMailAccess");
-//    return response(["message" => 'Registered','token' => $token->plainTextToken], 201);
-//});
-//
-//Route::post('/login', function () {
-//    $credentials = request()->only(['email', 'password']);
-//    return auth()->attempt($credentials);
-//});
-//ezt a tokent le kell menteni és header Authorization Bearer idebe
-
-//authenticate - it is now used with arrow function at apiResources
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 
