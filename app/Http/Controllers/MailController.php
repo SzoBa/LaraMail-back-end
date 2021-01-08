e26b262a70bf57c925d0a13d2c403a78b29ee83e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-01-08 02:22:13 +0100

    Add sent/received by user and save mail queries

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class MailController extends Controller
@@ -16,11 +18,21 @@ class MailController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return Response
      */
-    public function index()
+    public function index(Request $request): Response
     {
-        $mails =  Mail::all();
+//        This is the way of multiple filtering with or
+//        $mails =  DB::table('mails')->where('id_user_from', $request->user()->id)
+//        ->orWhere('id_user_to', $request->user()->id)->get();
+//          or see example beneath
+//        $mails = Mail::all()
+//            ->where('id_user_to', $request->user()->id)->sortBy('sent');
+        $mails = DB::table('mails')->join('users', 'mails.id_user_from', '=', 'users.id')
+            ->select('mails.*', 'users.name')
+            ->where('mails.id_user_to', '=', $request->user()->id)
+            ->get();
         return response($mails, 200);
     }
 
@@ -32,14 +44,25 @@ class MailController extends Controller
      */
     public function store(Request $request)
     {
-        $rules = ['id_user_from' => 'required|numeric', 'id_user_to' =>'required|numeric',
+        $rules = ['name' =>'required',
             'subject' => 'required|min:3', 'message' => 'required|min:3'];
         $validation = Validator::make($request->all(), $rules);
         if ($validation->fails()) {
             return response($validation->errors(), 400);
         }
-        return response(Mail::create($request->all()), 201);
-        //this could be with arrow function too, like ->json(data, status)
+        $destinationUser = User::where('name', $request->get('name'))->first();
+        if ($destinationUser) {
+            Mail::create(['id_user_from' =>$request->user()->id,
+                'id_user_to' => $destinationUser->id,
+                'subject' => $request->get('subject'),
+                'message' => $request->get('message'),
+            ]);
+            return response(['message'=> 'Send Successful!'], 201);
+            //this could be with arrow function too, like ->json(data, status)
+        }
+        return response(['message'=> 'Username doesn\'t exist in database!'], 404);
+
+
     }
 
     /**
@@ -86,5 +109,17 @@ class MailController extends Controller
         //number of deleted rows
         return $deletedRows === 0 ? response(["message"=>"No deletion executed!"], 404) :
             response(["message"=> "Record deleted!"], 204);
+    }
+
+    public function sent(Request $request): Response
+    {
+
+//        Mail::all()->where('id_user_from', $request->user()->id)->sortByDesc('sent');
+//        $mails = Mail::with('user')->get();
+        $mails = DB::table('mails')->join('users', 'mails.id_user_to', '=', 'users.id')
+        ->select('mails.*', 'users.name')
+        ->where('mails.id_user_from', '=', $request->user()->id)
+        ->get();
+        return response($mails, 200);
     }
 }
